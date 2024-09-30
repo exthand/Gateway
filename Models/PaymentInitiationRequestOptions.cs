4c4bfc5552b7f90e45f49bc903030e023b8f141a
Jean-Gabriel Debaille <jeangabriel@exthand.com>
2024-09-30 16:50:30 +0200

    Moved from Status Success code to OK (200)

--- Models/PaymentInitiationRequestOptions.cs
+++ Models/PaymentInitiationRequestOptions.cs
@@ -16,5 +16,47 @@ namespace Exthand.GatewayClient.Models
         public PaymentInitiationRequestOptionsType remittanceInformationUnstructured { get; set; } = PaymentInitiationRequestOptionsType.Optional;
         public PropertyConstrains remittanceInformationUnstructuredConstrains { get; set; } = new PropertyConstrains { maxLength = 140, minLength = 0, regex = "^[a-zA-Z0-9-/:().,? '+]{0,140}$", acceptedChars = "a-zA-Z0-9-/:().,? '+" };
 
+        public StructuredRemittanceOptions RemittanceInformationStructured { get; set; } = new StructuredRemittanceOptions();
+    }
+    
+    public class StructuredRemittanceOptions
+    {
+        public bool Supported { get; set; }
+        public PaymentInitiationRequestOptionsType Reference { get; set; } = PaymentInitiationRequestOptionsType.Required;
+        public PaymentInitiationRequestOptionsType Code { get; set; } = PaymentInitiationRequestOptionsType.Unused;
+        public PaymentInitiationRequestOptionsType Issuer { get; set; } = PaymentInitiationRequestOptionsType.Required;
+        public List<RemittanceIssuer> AllowedIssuers { get; set; }
+        public List<string> AllowedCodes { get; set; }
+        public PropertyConstrains ReferenceConstraints { get; set; } = new PropertyConstrains();
+        public PropertyConstrains CodeConstraints { get; set; } 
+        public PropertyConstrains IssuerConstraints { get; set; } 
+    }
+    
+    public enum RemittanceIssuer
+    {
+        /// <summary>
+        /// Belgium
+        /// </summary>
+        BBA,
+        /// <summary>
+        /// ISO 11649
+        /// </summary>
+        ISO,
+        /// <summary>
+        /// International Reference Number, same as ISO 11649
+        /// </summary>
+        INTL,
+        /// <summary>
+        /// Swedish Bankgiro OCR
+        /// </summary>
+        SEBG,
+        /// <summary>
+        /// Norwegian KID (OCR)
+        /// </summary>
+        NORF,
+        /// <summary>
+        /// Finnish reference number
+        /// </summary>
+        FIRF
     }
 }
